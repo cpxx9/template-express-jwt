4b995b453137c6f2b918e39d69f3755f5882ca39
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-04 10:18:17 -0500

    create db pool and populate script

---
+++ models/pool.js
@@ -0,0 +1,6 @@
+require('dotenv/config');
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+});
---
+++ models/populatedb.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+require('dotenv/config');
+const { Client } = require('pg');
+const { argv } = require('node:process');
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER GENERATED ALWAYS AS IDENTITY,
+  firstname VARCHAR ( 255 ),
+  lastname VARCHAR ( 255 ),
+  email VARCHAR ( 255 ),
+  admin BOOLEAN,
+  member BOOLEAN,
+  hash TEXT,
+  salt TEXT,
+  CONSTRAINT users_pkey PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  message_id INTEGER GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER NOT NULL,
+  title VARCHAR ( 128 ),
+  text TEXT,
+  created timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT messages_pkey PRIMARY KEY (message_id),
+  CONSTRAINT messages_user_fkey FOREIGN KEY(user_id)
+    REFERENCES users(user_id) MATCH SIMPLE
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT
+);
+`;
+
+const connectionString =
+  argv[2] ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+async function main() {
+  console.log('seeding...');
+  const client = new Client({
+    connectionString,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+}
+
+main();
