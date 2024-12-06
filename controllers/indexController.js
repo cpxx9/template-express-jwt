e248e24184b2cfa175d30277490185e2152a847e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-05 21:35:44 -0500

    started view author and date if member controls

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,17 @@
 const { getAllMessages, getUsersName } = require('../models/Messages');
 
 const displayMessages = async (req, res, next) => {
-  const messages = getAllMessages();
+  const messages = await getAllMessages();
 
-  res.render('index', {
-    messages,
-    member: req.user.member,
-    getUserName: getUsersName,
-  });
+  res.render(
+    'index',
+    {
+      messages,
+      member: req.user.member,
+      getUserName: getUsersName,
+    }
+    // { async: true }
+  );
 };
 
 module.exports = {
